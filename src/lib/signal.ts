9e977ad8656ada0f0568de02990cae3ea5f2f221
Ben Foxall <benfoxall@gmail.com>
2019-09-21 11:03:38 +0100

    Refactor ws stuff

--- src/lib/signal.ts
+++ src/lib/signal.ts
@@ -6,25 +6,56 @@ const TWILLIO_KEY = '{{twillio}}'
 
 type dataBack = (body: any, from: string) => void;
 
+const log = (...message: any[]) => {
+    console.log.apply(console, ['%c[ws]', 'color:#08f'].concat(message))
+}
+
 export class SignalClient {
+
     private ws: WebSocket;
     private listeners = new Set<dataBack>();
 
     constructor(public readonly uuid: string) {
         this.ws = new WebSocket(BACKEND)
 
-        this.ws.addEventListener('message', (m) => {
+        const ping = () => {
+            setTimeout(() => {
+                this._send({type: 'ping'}, 0)
+
+                ping()
+            }, 5 * 60 * 1000)
+        }
+        ping();
+
+
+        const token = sessionStorage.getItem(TOKEN_KEY_PREFIX + uuid)
+        const twillio = sessionStorage.getItem(TWILLIO_KEY)
+
+        const open = () => {
+            log('open')
+            this.ws.send(JSON.stringify({
+                type: 'setup',
+                uuid: uuid,
+                token
+            }))
+
+
+            // todo - or created at is hours old
+            if (!twillio) {
+                this.ws.send(JSON.stringify({
+                    type: 'twillio',
+                }))
+            }
+        }
+
+        const message = (m: MessageEvent) => {
+            log("message", m.data)
             try {
                 const data = JSON.parse(m.data);
 
-                console.log(">>>>", data);
-
                 if (data.type === 'setup' && data.token) {
-                    // this.handleSetup(data);
-
+                    log("Token updated")
                     sessionStorage.setItem(TOKEN_KEY_PREFIX + uuid, data.token)
-
-                    console.log("SET TOKEN")
                 }
 
                 if (data.type === 'twillio' && data.token) {
@@ -40,47 +71,16 @@ export class SignalClient {
             } catch (e) {
                 console.error(e);
             }
-        })
 
-        const token = sessionStorage.getItem(TOKEN_KEY_PREFIX + uuid)
-        const twillio = sessionStorage.getItem(TWILLIO_KEY)
+        }
 
-        this.ws.addEventListener("close", () => {
-            console.log("CLOSE")
+        const close = () => {
+            log("CLOSE")
+        }
 
-        })
-
-        this.ws.addEventListener("open", () => {
-            console.log("OPEN!!")
-            
-            this.ws.send(JSON.stringify({
-                type: 'setup',
-                uuid: uuid,
-                token
-            }))
-
-
-            // todo - or created at is hours old
-            if (!twillio) {
-                this.ws.send(JSON.stringify({
-                    type: 'twillio',
-                }))
-            }
-
-            const ping = () => {
-                setTimeout(() => {
-                    if (this.ws.readyState === WebSocket.OPEN) {
-                        this.ws.send(JSON.stringify({
-                            type: 'ping',
-                        }))
-                        ping()
-                    }
-                }, 5 * 60 * 1000)
-            }
-            ping();
-        })
-
-
+        this.ws.addEventListener('message', message);
+        this.ws.addEventListener('open', open);
+        this.ws.addEventListener('close', close);
     }
 
     on(data: "data", callback: dataBack) {
@@ -90,24 +90,34 @@ export class SignalClient {
     }
 
     send(target: string, message: any) {
-
-        if (this.ws.readyState === WebSocket.OPEN) {
-            this.ws.send(JSON.stringify({
-                type: 'send',
-                from: this.uuid,
-                to: target,
-                body: message
-            }))
-        } else {
-            throw new Error("WebSocket Closed")
-        }
-
+        this._send({
+            type: 'send',
+            from: this.uuid,
+            to: target,
+            body: message
+        }, 0)
     }
 
     disconnect() {
         throw new Error("Not Implemented")
     }
 
-    
+    /** Try and send to the underlying connection */
+    private _send(message: any, retries = 3) {
+        if (this.ws.readyState === WebSocket.OPEN) {
+            this.ws.send(JSON.stringify(message))
+            return true
 
+        } else if(retries > 0) {
+            log("Retrying")
+            
+            setTimeout(
+                this._send.bind(this),
+                1500,
+                message,
+                retries - 1
+            )
+        
+        }
+    }
 }
