9f8aaac84d20d984ce083ed16168bd5505612f7c
Ben Foxall <benfoxall@gmail.com>
2020-06-22 01:42:07 +0100

    refactor hooks

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,49 +1,29 @@
-import { useState, useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { SignalClient } from "./signal";
 import { useDispatch } from "react-redux";
 import { PeerServiceServer, PeerServiceClient } from "./peerService";
 
 export const useSignal = (uuid: string) => {
-  const [connection, setConnection] = useState<SignalClient>();
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const client = new SignalClient(uuid, dispatch);
-
-    setConnection(client);
-
-    (window as any).client = client;
-
-    return () => {
-      client.disconnect();
-    };
-  }, [uuid]);
+  const connection = useMemo(() => new SignalClient(uuid, dispatch), [uuid]);
+  useEffect(() => () => connection.disconnect(), [connection]);
 
   return connection;
 };
 
 export const usePeerServer = (uuid: string) => {
-  const [server, setServer] = useState<PeerServiceServer>();
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const server = new PeerServiceServer(uuid, dispatch);
-
-    setServer(server);
-  }, [uuid]);
+  const server = useMemo(() => new PeerServiceServer(uuid, dispatch), [uuid]);
 
   return server;
 };
 
 export const usePeerClient = (uuid: string) => {
-  const [client, setClient] = useState<PeerServiceClient>();
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const client = new PeerServiceClient(uuid, dispatch);
-
-    setClient(client);
-  }, [uuid]);
+  const client = useMemo(() => new PeerServiceClient(uuid, dispatch), [uuid]);
 
   return client;
 };
